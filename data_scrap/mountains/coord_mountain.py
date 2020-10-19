701c6cc0e443842531c1d0fa7b396f2eb3cb2b31
GEEZEEBE <geezeebe@gmail.com>
2020-10-19 21:45:21 +0900

    improvement coordination accuracy (1st trial)

--- data_scrap/mountains/coord_mountain.py
+++ data_scrap/mountains/coord_mountain.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from datetime import datetime
 
-from kakaoapi import by_keyword
+from kakaoapi import by_keyword, by_keyword_AT4
 import mongoDB
 import mysqlDB
 
@@ -16,12 +16,21 @@ mt_img_table = 'mt_images'                        # mariaDB
 
 def get_coord(mt):
 
-    keyword = mt['name'] + ' ' + mt['site'].replace(',', ' ').split()[0]
+    keyword = mt['name']
     print(keyword)
-    # print(get_mountain_info2(keyword))
-    # print(apiByKeyword(keyword))
+    data = by_keyword_AT4(keyword)
+    if data['meta']['total_count'] == 0:
+        keyword = mt['name'] + ' ' + mt['site'].replace(',', ' ').split()[0]
+        print(keyword)
+        data = by_keyword(keyword)
 
-    data = by_keyword(keyword)
+    area = ['강원', '충북', '충남', '경북', '경남', '전북', '울산', '전남', '경기']
+
+    for d in data['documents']:
+        if d['address_name'][:2] in area and d['phone'] == "":
+            print(d['x'], d['y'])
+            return d['x'], d['y']
+
     print(data['documents'][0]['x'], data['documents'][0]['y'])
 
     return data['documents'][0]['x'], data['documents'][0]['y']
--- data_scrap/mountains/kakaoapi.py
+++ data_scrap/mountains/kakaoapi.py
@@ -20,3 +20,23 @@ def by_keyword(keyword):
     # print(json.dumps(data, ensure_ascii=False, indent=3))
 
     return data
+
+
+def by_keyword_AT4(keyword):
+    KakaoAK ='KakaoAK 8bca89540b17aa5ea6811b24f889fba3'
+
+    encText = quote(keyword)
+    url = "https://dapi.kakao.com/v2/local/search/keyword.json?page=1&size=15&sort=accuracy&category_group_code=AT4"
+    url = url + "&query=" + encText
+
+    header = {
+        "Authorization":KakaoAK
+    }
+    print(url)
+
+    response = requests.get(url, headers=header)
+    print(response.status_code)
+    data = json.loads(response.content)
+    # print(json.dumps(data, ensure_ascii=False, indent=3))
+
+    return data
